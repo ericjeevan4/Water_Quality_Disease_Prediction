9f49ae4565d9965d15aa5136abbfe2309f5b292a
ericjeevan4 <ericjeevan4@gmail.com>
2026-09-10 14:45:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import joblib
 import pandas as pd
 import numpy as np
-from src.shap_flask_explanation import get_shap_explanation
+from shap_flask_explanation import get_shap_explanation
 
 app = Flask(__name__)
 
