0b34d2b7e839df6e743f2ffb87ca93656f906e92
ericjeevan4 <ericjeevan4@gmail.com>
2026-09-10 16:24:21 +0530

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -273,10 +273,11 @@ def api_predict():
             )
 
         # SHAP explanation
-        shap_explanations = get_shap_explanation(
+        '''shap_explanations = get_shap_explanation(
             input_df,
             prediction
-        )
+        )'''
+        shap_explanations = []
 
         return {
             "predicted_disease": prediction,
